2197952aa5db02c156d56a079673a5818a7ef08d
chaitanyabhajibhakare <chaitanyabhajibhakare@gmail.com>
2017-01-12 23:37:17 -0800

    extra files added

--- app/src/main/java/app/techieni3/com/myapplication/MainActivity.java
+++ app/src/main/java/app/techieni3/com/myapplication/MainActivity.java
@@ -1,46 +1,13 @@
 package app.techieni3.com.myapplication;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.TextView;
+import android.support.v7.app.ActionBarActivity;
 
-import static java.lang.Thread.sleep;
-
-public class MainActivity extends AppCompatActivity {
-
-    TextView textView;
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        textView = (TextView) findViewById(R.id.text);
-
-        Thread th = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                        try {
-                            sleep(3*1000);
-
-                            finish();
-
-                        }catch (Exception e){
-                            e.printStackTrace();
-                        }
-            }
-        });
-
-        Typeface tf = Typeface.createFromAsset(getAssets(),"scriptmtbold.ttf");
-
-
-
-        Animation myAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.my_anim);
-        textView.setTypeface(tf);
-        textView.startAnimation(myAnim);
-
     }
 }
---
+++ app/src/main/java/app/techieni3/com/myapplication/Splash.java
@@ -0,0 +1,51 @@
+package app.techieni3.com.myapplication;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import static java.lang.Thread.sleep;
+
+public class Splash extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        textView = (TextView) findViewById(R.id.text);
+
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                        try {
+                            sleep(3*1000);
+                            Intent i = new Intent(Splash.this,MainActivity.class);
+                            startActivity(i);
+                            finish();
+
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+            }
+
+
+        });
+            th.start();
+        Typeface tf = Typeface.createFromAsset(getAssets(),"scriptmtbold.ttf");
+
+
+
+        Animation myAnim = AnimationUtils.loadAnimation(Splash.this, R.anim.my_anim);
+        textView.setTypeface(tf);
+        textView.startAnimation(myAnim);
+
+
+    }
+}
