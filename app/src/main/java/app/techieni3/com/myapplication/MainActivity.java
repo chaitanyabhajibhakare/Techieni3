7be843faf30b67474c94ca8e7b2e0d1c033a5187
chaitanyabhajibhakare <chaitanyabhajibhakare@gmail.com>
2017-02-02 15:06:36 +0530

    Some changes

--- app/src/main/java/app/techieni3/com/myapplication/MainActivity.java
+++ app/src/main/java/app/techieni3/com/myapplication/MainActivity.java
@@ -1,14 +1,14 @@
 package app.techieni3.com.myapplication;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
-    Button viewpageActivity;
+    Button viewpageActivity, it;
 
 
 
@@ -17,12 +17,25 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewpageActivity = (Button) findViewById(R.id.electronics);
+        it= (Button) findViewById(R.id.it);
 
         viewpageActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,CustomPagerAdapter.class);
+                Intent i = new Intent(MainActivity.this,Main2Activity.class);
                 startActivity(i);
+            }
+        });
+
+        it.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent i = new Intent(MainActivity.this,ActivityOnIt.class);
+                startActivity(i);
+
+
             }
         });
     }
